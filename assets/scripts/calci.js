0c13d278008ec7405cbb6214a04ffb104c0e7858
Prem Patel <prem.patel3105@gmail.com>
2015-09-05 12:26:53 +0530

    basic calculator

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-	$('#keys .digit').click(function(){
-		$('#preview').html($('#preview').html() + this.dataset.digit);
-	});
+	$('#calculator .input').click(function(){
+		var input;
+		if(this.dataset.keyType == "digit"){
+			input = this.dataset.digit;
+		} else if(this.dataset.keyType == "operator"){			
+			input = this.dataset.operator;
+		} else if(this.dataset.keyType == "delete"){
+			$('#preview').html($('#preview').html().slice(0, -1));	
+		} else if(this.dataset.keyType == "equals"){
+			$('#result').html(eval($('#preview').html()));
+		}
 
-	$('#delete').click(function(){
-		$('#preview').html($('#preview').html().slice(0, -1));
+		if(typeof(input) != 'undefined'){
+			$('#preview').html($('#preview').html() + input);
+		}
+		
 	});
+	
+
 });
