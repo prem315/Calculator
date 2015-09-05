266a6d1708193f2d2bde47ceacd21aeba242cec2
Prem Patel <prem.patel3105@gmail.com>
2015-09-05 16:07:08 +0530

    modularizing script of calculator

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,31 +1,47 @@
-$(document).ready(function(){
-	$('#calculator .input').click(function(){
-		var input;
-		if(this.dataset.keyType == "digit"){
-			input = this.dataset.digit;
-		} else if(this.dataset.keyType == "operator"){			
-			input = this.dataset.operator;
-		} else if(this.dataset.keyType == "delete"){
-			$('#preview').html($('#preview').html().slice(0, -1));	
-		} else if(this.dataset.keyType == "equals"){
-			$('#result').html(eval($('#preview').html()));
-		}
+var Calci = {
 
-		if(typeof(input) != 'undefined'){
-			$('#preview').html($('#preview').html() + input);
-		}
+	init: function(){
+		$('#calculator .input').click(function(){
 		
-	});
-	
-	$('#calculator #delete').dblclick(function(){
-		$('#preview').html('');
-		$('#result').html('');
-	});
-
-	['0','1','2','3','4','5','6','7','8','9'].forEach(function(digit){
-		$(document).bind('keyup', digit, function(){
-			$('#preview').html($('#preview').html() + digit);
+			if(this.dataset.keyType == "digit"){
+				Calci.handleInput(this.dataset.digit);
+			} else if(this.dataset.keyType == "operator"){			
+				Calci.handleInput(this.dataset.operator);
+			} else if(this.dataset.keyType == "delete"){
+				Calci.handleDelete();	
+			} else if(this.dataset.keyType == "equals"){
+				Calci.evaluateResult();
+			}
+		
 		});
-		// $('#preview').html($('#preview').html() + digit);
-	});
+
+		$('#calculator #delete').dblclick(function(){
+			$('#preview').html('');
+			$('#result').html('');
+		});
+
+		['0','1','2','3','4','5','6','7','8','9'].forEach(function(digit){
+			$(document).bind('keyup', digit, function(){
+				Calci.handleInput(digit);
+			});
+		});
+	},
+
+	handleInput: function(input){
+		$('#preview').html($('#preview').html()+input);
+	},
+
+	handleDelete: function(){
+		$('#preview').html($('#preview').html().slice(0, -1));
+	},
+
+	evaluateResult: function(){
+		$('#result').html(eval($('#preview').html()));	
+	}
+}
+
+
+
+$(document).ready(function(){
+	Calci.init();
 }); 
